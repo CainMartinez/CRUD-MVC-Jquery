f9c605cbf6a9573f1c46291b697bd3302ac59b99
Cain <cainmartinez2@hotmail.com>
2023-12-13 15:57:53 +0100

    fix update

--- update.php
+++ update.php
@@ -16,6 +16,7 @@ while($res = mysqli_fetch_array($result))
     $characteristics = $res['characteristics']; 
     $price = $res['price']; 
 }
+$characteristics = explode(', ', $characteristics);
 ?>
 <html>
 <head>	
@@ -52,13 +53,42 @@ while($res = mysqli_fetch_array($result))
             <td><input type="text" name="square_meters" pattern="\d*" value="<?php echo $square_meters;?>"></td>
         </tr>
         <tr> 
-            <td>Property Type</td>
-            <td><input type="text" name="property_type" value="<?php echo $property_type;?>"></td>
-        </tr>
-        <tr> 
-            <td>Number of Rooms</td>
-            <td><input type="text" name="number_of_rooms" value="<?php echo $number_of_rooms;?>"></td>
-        </tr>
+                    <td>Property Type</td>
+                    <td>
+                        <input type="radio" id="house" name="property_type" value="House" <?php echo $property_type == 'House' ? 'checked' : ''; ?> required>
+                        <label for="house">House</label><br>
+                        <input type="radio" id="apartment" name="property_type" value="Apartment" <?php echo $property_type == 'Apartment' ? 'checked' : ''; ?>>
+                        <label for="apartment">Apartment</label><br>
+                        <input type="radio" id="other" name="property_type" value="Other" <?php echo $property_type == 'Other' ? 'checked' : ''; ?>>
+                        <label for="other">Other</label>
+                    </td>
+                </tr>
+                <tr> 
+                    <td>Characteristics</td>
+                    <td>
+                        <input type="checkbox" id="balcony" name="characteristics[]" value="Balcony" <?php echo in_array('Balcony', $characteristics) ? 'checked' : ''; ?>>
+                        <label for="balcony">Balcony</label><br>
+                        <input type="checkbox" id="garage" name="characteristics[]" value="Garage" <?php echo in_array('Garage', $characteristics) ? 'checked' : ''; ?>>
+                        <label for="garage">Garage</label><br>
+                        <input type="checkbox" id="pool" name="characteristics[]" value="Pool" <?php echo in_array('Pool', $characteristics) ? 'checked' : ''; ?>>
+                        <label for="pool">Pool</label><br>
+                        <input type="checkbox" id="garden" name="characteristics[]" value="Garden" <?php echo in_array('Garden', $characteristics) ? 'checked' : ''; ?>>
+                        <label for="garden">Garden</label><br>
+                    </td>
+                </tr>
+                <tr> 
+                    <td>Number of Rooms</td>
+                    <td>
+                        <select name="number_of_rooms" required>
+                            <option value="">Select number of rooms</option>
+                            <option value="1" <?php echo $number_of_rooms == '1' ? 'selected' : ''; ?>>1</option>
+                            <option value="2" <?php echo $number_of_rooms == '2' ? 'selected' : ''; ?>>2</option>
+                            <option value="3" <?php echo $number_of_rooms == '3' ? 'selected' : ''; ?>>3</option>
+                            <option value="4" <?php echo $number_of_rooms == '4' ? 'selected' : ''; ?>>4</option>
+                            <option value="5" <?php echo $number_of_rooms == '5' ? 'selected' : ''; ?>>5</option>
+                        </select>
+                    </td>
+                <tr> 
         <tr> 
             <td>Comment</td>
             <td><textarea name="comment"><?php echo $comment;?></textarea></td>
@@ -66,10 +96,6 @@ while($res = mysqli_fetch_array($result))
         <tr> 
             <td>Date of Publication</td>
             <td><input type="text" name="date_publication" id="date_publication" value="<?php echo $date_publication;?>"></td>
-        <tr> 
-            <td>Characteristics</td>
-            <td><input type="text" name="characteristics" value="<?php echo $characteristics;?>"></td>
-        </tr>
         <tr> 
             <td>Price</td>
             <td><input type="text" name="price" pattern="[0-9]*" value="<?php echo $price;?>"></td>
